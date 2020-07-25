9bd0ab39c14848b0f5f89af017bef9bb82427be7
kardelen-karatas <kardelen.karatas@gmail.com>
2020-07-25 11:13:01 +0200

    anim loop problem

--- js/plankton.js
+++ js/plankton.js
@@ -17,12 +17,30 @@ class Plankton{
 
     selectRandomPosition(){
         const randomNum = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
-        let x = randomNum(1, 15)
-        let y = randomNum(1, 15)        
+        let x = randomNum(1, 14)
+        let y = randomNum(1, 14)        
+        //console.log(`lab : ${labyrinth}, tileType : ${labyrinth.tileType(x,y)}`)
+        
         if(labyrinth.tileType(x,y) === "F"){
+            console.log(`planktons : ${planktons}`)
+
             this.x = x * this.w
             this.y = y * this.h
-        } else {this.selectRandomPosition()}
+            if(planktons) {
+            
+                for(const id in planktons){
+                 
+                    let plankton = planktons[id]
+                    console.log(`existing planton ${id} : ${plankton.x /48}, ${plankton.y/48}`)
+                    console.log(`Newly plankton : ${x}, ${y}`)
+                    if(plankton.x === x && plankton.y === y){
+                        console.log("test") 
+                    }
+                }
+        } 
+
+        }
+        else {this.selectRandomPosition()}
         
     }
 
